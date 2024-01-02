7e36d603353f116323507ea7d73bbf332a037445
ryanrhoads <rrhoads@graymatters-inc.com>
2024-01-02 11:22:19 -0500

    Add websocket logic to tictactoe

--- src/pages/games/TicTacToe.jsx
+++ src/pages/games/TicTacToe.jsx
@@ -1,9 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+import useWebsocket from 'react-use-websocket'
+import throttle from 'lodash.throttle'
+
 import Modal from "../../components/shared/Modal"
 
 const TicTacToe = (props) => {
-    const [currentPlayer, setCurrentPlayer] = React.useState(true);
     const [board, setBoard] = React.useState({1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"",})
+
+    const WS_URL = 'ws://localhost:3773/ws/game'
+    const THROTTLE = 50
+
+    const { sendJsonMessage, lastJsonMessage } = useWebsocket(WS_URL, {
+        // queryParams: { username }
+    })
+
+    const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
+
+    function createBoard() {
+        let array = []
+
+        for (let i = 0; i < 3; i++) {
+            array.push(
+                <div key={i} id={`row${i}`} className="flex justify-center">
+                    {createRow(i)}
+                </div>
+            )
+        }
+
+        return array
+    }
+
+    function createRow(index) {
+        let array = []
+
+        for (let i = 0; i < 3; i++) {
+            let btnIndex = i + (index * 3) + 1
+            array.push(
+                <button 
+                key={btnIndex} 
+                id={`btn${btnIndex}`} 
+                className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
+                onClick={() => { submitMove(btnIndex) }}
+                >
+                    {board[btnIndex]}
+                </button>
+            )
+        }
+
+        return array
+    }
+
+    function submitMove(index) {
+        console.log(`Button ${index} pressed`)
+        sendJsonMessageThrottled.current({
+            username: "ryan",
+        })
+    }
+
+    if(lastJsonMessage) {
+        console.log("Message: ", lastJsonMessage)
+    }
 
     return (
         <div className="z-50"> 
@@ -15,113 +72,25 @@ const TicTacToe = (props) => {
                                 <h1 className='text-2xl font-montserrat font-semibold text-slate-700 text-center'>
                                     Tic-Tac-Toe
                                 </h1>
+                                <div>
+                                    <button
+                                    id={"btn2"} 
+                                    className="flex justify-center align-middle w-56 h-8 m-2 mt-8 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
+                                    onClick={() => {
+                                        console.log("Looking for Opponent...")
+                                    }}
+                                    >
+                                        Find Opponent
+                                    </button>
+                                </div>
                                 <div className="flex-none w-full rounded-xl mt-6">
-                                    {/* {createBoard(3)} */}
-                                    <div id="row1" className="flex justify-center">
-                                        <button
-                                        id={"btn1"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            if(tempArray[1] == ""){
-                                                setBoard({...board, 1: currentPlayer ? "X" : "O"}) 
-                                                setCurrentPlayer(!currentPlayer)
-                                            }
-                                        }}
-                                        >
-                                            {board[1]}
-                                        </button>
-                                        <button
-                                        id={"btn2"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 2: currentPlayer ? "X" : "O"}) 
-                                            setCurrentPlayer(!currentPlayer)
-                                        }}
-                                        >
-                                            {board[2]}
-                                        </button>
-                                        <button
-                                        id={"btn3"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 3:"X"}) 
-                                        }}
-                                        >
-                                            {board[3]}
-                                        </button>
-                                    </div>
-                                    <div id="row2" className="flex justify-center">
-                                        <button
-                                        id={"btn4"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 4:"X"}) 
-                                        }}
-                                        >
-                                            {board[4]}
-                                        </button>
-                                        <button
-                                        id={"btn5"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 5:"X"}) 
-                                        }}
-                                        >
-                                            {board[5]}
-                                        </button>
-                                        <button
-                                        id={"btn6"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 6:"X"}) 
-                                        }}
-                                        >
-                                            {board[6]}
-                                        </button>
-                                    </div>
-                                    <div id="row3" className="flex justify-center">
-                                        <button
-                                        id={"btn7"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 7:"X"}) 
-                                        }}
-                                        >
-                                            {board[7]}
-                                        </button>
-                                        <button
-                                        id={"btn8"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 8:"X"}) 
-                                        }}
-                                        >
-                                            {board[8]}
-                                        </button>
-                                        <button
-                                        id={"btn9"} 
-                                        className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                        onClick={() => { 
-                                            let tempArray = board
-                                            setBoard({...board, 9:"X"}) 
-                                        }}
-                                        >
-                                            {board[9]}
-                                        </button>
-                                    </div>
+                                    { createBoard() }
                                 </div>
                                 <div>
                                     <button
                                     id={"btn2"} 
                                     className="flex justify-center align-middle w-56 h-8 m-2 mt-8 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
+                                    onClick={() => { setBoard({1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"",}) }}
                                     >
                                         RESET GAME
                                     </button>
@@ -134,39 +103,5 @@ const TicTacToe = (props) => {
         </div>
     )
 }
-
-
-// function createBoard(size) {
-//     let array = []
-//     for (let i = 0; i < size; i++) {
-//         array.push(
-//             <div key={i} id={`row${i}`} className="flex justify-center">
-//                 {createRow(size, i)}
-//             </div>
-//         )
-//     }
-
-//     return array
-// }
-
-// function createRow(size, index) {
-//     let array = []
-
-//     for (let i = 0; i < size; i++) {
-//         let btnIndex = i + (index * 3) + 1
-//         array.push(
-//             <button 
-//             key={btnIndex} 
-//             id={`btn${btnIndex}`} 
-//             className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-//             onClick={() => { }}
-//             >
-//                 {""}
-//             </button>
-//         )
-//     }
-
-//     return array
-// }
 
 export default TicTacToe
