551a6481ce2d7051de1e50febc297b387c46b5a4
ryanrhoads <rrhoads@graymatters-inc.com>
2024-01-08 09:06:39 -0500

    Add logic for entering usernames and conecting with opponent

--- src/pages/games/Games.jsx
+++ src/pages/games/Games.jsx
@@ -3,12 +3,16 @@ import React, { useState, useRef } from "react";
 import useWebsocket from 'react-use-websocket'
 import throttle from 'lodash.throttle'
 
+import { TextField } from '@mui/material'
+
 import TicTacToe from "./TicTacToe"
 import Chess from "./Chess"
 
 const Games = () => {
     const [showTTTModal, setShowTTTModal] = useState(false);
     const [showChessModal, setShowChessModal] = useState(false);
+    const [tempUsername, setTempUsername] = React.useState("")
+    const [username, setUsername] = React.useState("")
 
     const WS_URL = 'ws://localhost:3773/ws/message'
     const THROTTLE = 50
@@ -19,7 +23,7 @@ const Games = () => {
 
     const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
 
-    const logReuest = req => {
+    const logRequest = req => {
         Object.keys(req).map(() => {
             console.log("From: ", req.from)
             console.log("To: ", req.to)
@@ -37,35 +41,54 @@ const Games = () => {
     }
 
     if(lastJsonMessage) {
-        logReuest(lastJsonMessage)
+        logRequest(lastJsonMessage)
     }
 
     return(
         <div>
             <h1 className="text-center p-4"> Games Page </h1>
-            <p className="mx-6"> Click on a game to play! </p>
 
-            <div>
-                <div className="flex w-full m-6 text-xl text-center font-semibold">
-                    <button 
-                    className="w-36 mr-6 px-4 py-2 border-2 border-slate-700 rounded-xl bg-slate-300 shadow-lg hover:bg-slate-200 hover:border-slate-600"
-                    onClick={() => {testWebSocket()}}>
-                        Test Web Socket
-                    </button>
-                    <button 
-                    className="w-36 mr-6 px-4 py-2 border-2 border-slate-700 rounded-xl bg-slate-300 shadow-lg hover:bg-slate-200 hover:border-slate-600"
-                    onClick={() => {setShowTTTModal(true)}}>
-                        Tic Tac Toe
-                    </button>
-                    <button 
-                    className="w-36 px-4 py-2 border-2 border-slate-700 rounded-xl bg-slate-300 shadow-lg hover:bg-slate-200 hover:border-slate-600"
-                    onClick={() => {setShowChessModal(true)}}>
-                        Chess
+            {username == "" ? 
+            <>
+                <p className="mx-6"> Enter your Username to continue! </p>
+                <div className="flex">
+                    <div>Username: </div>
+                    <TextField
+                        className=""
+                        onChange={(e) => {
+                            setTempUsername(e.target.value)
+                        }}
+                    />
+                    <button onClick={() => {setUsername(tempUsername)}}>
+                        Submit
                     </button>
                 </div>
-            </div>
-            <TicTacToe showModal={showTTTModal} setShowModal={setShowTTTModal}/>
-            <Chess showModal={showChessModal} setShowModal={setShowChessModal}/>
+            </> : 
+            <>
+                <p className="mx-6"> {`Hello ${username}! Click on a game to play!`} </p>
+                <div>
+                    <div className="flex w-full m-6 text-xl text-center font-semibold">
+                        <button 
+                        className="w-36 mr-6 px-4 py-2 border-2 border-slate-700 rounded-xl bg-slate-300 shadow-lg hover:bg-slate-200 hover:border-slate-600"
+                        onClick={() => {testWebSocket()}}>
+                            Test Web Socket
+                        </button>
+                        <button 
+                        className="w-36 mr-6 px-4 py-2 border-2 border-slate-700 rounded-xl bg-slate-300 shadow-lg hover:bg-slate-200 hover:border-slate-600"
+                        onClick={() => {setShowTTTModal(true)}}>
+                            Tic Tac Toe
+                        </button>
+                        <button 
+                        className="w-36 px-4 py-2 border-2 border-slate-700 rounded-xl bg-slate-300 shadow-lg hover:bg-slate-200 hover:border-slate-600"
+                        onClick={() => {setShowChessModal(true)}}>
+                            Chess
+                        </button>
+                    </div>
+                </div>
+            </>}
+            
+            {username == "" ? <></> : <TicTacToe showModal={showTTTModal} setShowModal={setShowTTTModal} username={username}/>}
+            <Chess showModal={showChessModal} setShowModal={setShowChessModal} username={username}/>
         </div>
     )
 }
--- src/pages/games/TicTacToe.jsx
+++ src/pages/games/TicTacToe.jsx
@@ -6,9 +6,13 @@ import throttle from 'lodash.throttle'
 import Modal from "../../components/shared/Modal"
 
 const TicTacToe = (props) => {
-    const [board, setBoard] = React.useState({1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"",})
+    const [gameBoard, setGameBoard] = React.useState(["", "", "", "", "", "", "", "", ""])
+    const [gameId, setGameId] = React.useState("")
+    const [opponent, setOpponent] = React.useState("")
+    const [username, setUsername] = React.useState(props.username)
 
-    const WS_URL = 'ws://localhost:3773/ws/game'
+    const GAME_TYPE = "tictactoe"
+    const WS_URL = `ws://localhost:3773/ws/game/${GAME_TYPE}`
     const THROTTLE = 50
 
     const { sendJsonMessage, lastJsonMessage } = useWebsocket(WS_URL, {
@@ -16,6 +20,36 @@ const TicTacToe = (props) => {
     })
 
     const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
+
+    useEffect(() => {
+        if(lastJsonMessage !== null) {
+            if(lastJsonMessage.board){
+                setGameBoard(lastJsonMessage.board)
+            }
+            
+            if(lastJsonMessage.id){
+                setGameId(lastJsonMessage.id)
+            }
+
+            if(lastJsonMessage.pX || lastJsonMessage.pO) {
+                if(lastJsonMessage.pX == username) {
+                    setOpponent(lastJsonMessage.pO)
+                } else {
+                    setOpponent(lastJsonMessage.pX)
+                }
+
+            }
+        }
+
+    }, [lastJsonMessage])
+
+    // useEffect(() => {
+    //     console.log("Game ID: " + gameId)
+    // }, [gameId])
+
+    // useEffect(() => {
+    //     console.log("Game Board: " + JSON.stringify(gameBoard))
+    // }, [gameBoard])
 
     function createBoard() {
         let array = []
@@ -43,7 +77,7 @@ const TicTacToe = (props) => {
                 className="w-16 h-16 m-2 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
                 onClick={() => { submitMove(btnIndex) }}
                 >
-                    {board[btnIndex]}
+                    {gameBoard[btnIndex]}
                 </button>
             )
         }
@@ -51,53 +85,83 @@ const TicTacToe = (props) => {
         return array
     }
 
-    function submitMove(index) {
-        console.log(`Button ${index} pressed`)
+    function headerButton() {
+        if(gameId == "") {
+            return (
+                <>
+                    <button
+                    id={"btn2"} 
+                    className="flex justify-center align-middle w-56 h-8 m-2 mt-8 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
+                    onClick={() => { findOpponent() }}
+                    >
+                        Find Opponent
+                    </button>
+                </>
+            )
+        } else if(opponent == "") {
+            return (
+                <div className="h-8 mt-6 mb-8">
+                    Waiting for Opponent to Connect 
+                    <div className="flex items-center justify-center space-x-2 animate-spin">
+                        <div className="border-t-transparent border-solid animate-spin rounded-full border-white-400 border-4 h-8 w-8"></div>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <>
+                    {`Opponent: ${opponent}`}
+                </>
+            )
+        }
+    }
+
+    function findOpponent() {
+        console.log("Connecting you with an opponent...")
         sendJsonMessageThrottled.current({
-            username: "ryan",
+            action: "connect",
+            data: "",
+            gameId: `${gameId}`,
+            username: `${username}`,
         })
     }
 
-    if(lastJsonMessage) {
-        console.log("Message: ", lastJsonMessage)
+    function submitMove(index) {
+        console.log(`Button ${index} pressed`)
+        sendJsonMessageThrottled.current({
+            action: "move",
+            data: `${index}`,
+            gameId: `${gameId}`,
+            username: `${username}`,
+        })
     }
 
     return (
         <div className="z-50"> 
-            <Modal showModal={props.showModal} setShowModal={props.setShowModal} setBoard={setBoard}>
+            <Modal showModal={props.showModal} setShowModal={props.setShowModal} setBoard={setGameBoard}>
                 <div className="p-5 pl-16 rounded-xl">
-                    <>
-                        <div>
-                            <div className='flex-none w-full rounded-xl pb-6'>
-                                <h1 className='text-2xl font-montserrat font-semibold text-slate-700 text-center'>
-                                    Tic-Tac-Toe
-                                </h1>
-                                <div>
-                                    <button
-                                    id={"btn2"} 
-                                    className="flex justify-center align-middle w-56 h-8 m-2 mt-8 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                    onClick={() => {
-                                        console.log("Looking for Opponent...")
-                                    }}
-                                    >
-                                        Find Opponent
-                                    </button>
-                                </div>
-                                <div className="flex-none w-full rounded-xl mt-6">
-                                    { createBoard() }
-                                </div>
-                                <div>
-                                    <button
-                                    id={"btn2"} 
-                                    className="flex justify-center align-middle w-56 h-8 m-2 mt-8 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
-                                    onClick={() => { setBoard({1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"",}) }}
-                                    >
-                                        RESET GAME
-                                    </button>
-                                </div>
+                    <div>
+                        <div className='flex-none w-full rounded-xl pb-6'>
+                            <h1 className='text-2xl font-montserrat font-semibold text-slate-700 text-center'>
+                                Tic-Tac-Toe
+                            </h1>
+                            <div>
+                                { headerButton() }
+                            </div>
+                            <div className="flex-none w-full rounded-xl mt-6">
+                                { createBoard() }
+                            </div>
+                            <div>
+                                <button
+                                id={"btn2"} 
+                                className="flex justify-center align-middle w-56 h-8 m-2 mt-8 border-2 border-slate-500 rounded-lg bg-slate-300 shadow-lg hover:bg-slate-200"
+                                onClick={() => { setGameBoard({1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"",}) }}
+                                >
+                                    QUIT GAME
+                                </button>
                             </div>
                         </div>
-                    </>
+                    </div>
                 </div>
             </Modal>
         </div>
